086fa6be79b2be4bb01e41b352f17a85df3b1d68
An0nym0us92 <dash.prabhuduttpd1@gmail.com>
2025-08-18 03:49:52 +0530

    Added 11.c

---
+++ 11.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+  float a,ar,v;
+  printf("Enter side ");
+  scanf("%f",&a);
+  ar=6*a*a;
+  v=a*a*a;
+  printf("Area = %f",ar);
+  printf("\n Volume = %f",v);
+
+}
