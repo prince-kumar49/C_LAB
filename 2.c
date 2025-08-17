7048bf025c170f9460ea6b62a0dc952a48454854
Prince Kumar <robertprincekumar@gmail.com>
2025-08-18 03:40:34 +0530

    added 2.c

---
+++ 2.c
@@ -0,0 +1,12 @@
+/*Input 2 values with the help of user and add them */
+#include<stdio.h>
+void main()
+{
+  /*Declare the variables*/
+  int a=5,b=7,c;
+  /* Logic */
+  c=a+b;
+  /* Display the output*/
+  printf("Sum = %d",c);
+
+}
